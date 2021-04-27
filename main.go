866c78ff19f1277b83cc3c7fc0506f48dd527e5b
danyollee <leed3@oregonstate.edu>
2021-04-27 13:42:03 -0400

    update map to string->string. iterate thru each char of string

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 )
 
 // fills map to convert int to string output
-func fill_map(m map[int]string) {
+func fill_map(m map[string]string) {
 
-	m[0] = "Zero"
-	m[1] = "One"
-	m[2] = "Two"
-	m[3] = "Three"
-	m[4] = "Four"
-	m[5] = "Five"
-	m[6] = "Six"
-	m[7] = "Seven"
-	m[8] = "Eight"
-	m[9] = "Nine"
+	m["0"] = "Zero"
+	m["1"] = "One"
+	m["2"] = "Two"
+	m["3"] = "Three"
+	m["4"] = "Four"
+	m["5"] = "Five"
+	m["6"] = "Six"
+	m["7"] = "Seven"
+	m["8"] = "Eight"
+	m["9"] = "Nine"
 
 }
 
@@ -26,16 +26,18 @@ func main() {
 
 	// create a test array
 	array := [6]int{1,3,5,7,9, 35}
-	fmt.Println(array)
 
 	// create and fill map of int->string
-	m:= make(map[int]string)
+	m:= make(map[string]string)
 
 	fill_map(m)
 
-
-	for _, val := range array {
-		fmt.Println(strconv.Itoa(val))
-		// fmt.Println(m[val])
+	fmt.Println(array)
+	for _, v := range(array) {
+		s := strconv.Itoa(v)
+		for _, c := range(s) {
+			fmt.Println(m[string(c)])
+		}
 	}
+
 }
